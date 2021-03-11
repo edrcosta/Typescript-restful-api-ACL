f3b85205b5b2f65cb25c515d96003fab1d1286b9
Eder Costa <edercosta@oulook.com>
2021-03-10 21:11:13 -0300

    authentication controller blank

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express'
+
+export class AuthenticationController
+{
+    generateToken(req: Request, res: Response){
+        res.json({
+            status: 'success',
+            method: 'get'
+        })
+    }
+}
