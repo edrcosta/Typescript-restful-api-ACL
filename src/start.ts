2ef8620c00595fe2715b4df26af3834af11af89a
Eder Costa <edercosta@oulook.com>
2021-03-11 23:17:55 -0300

    initialize database

--- src/start.ts
+++ src/start.ts
@@ -3,19 +3,25 @@ import * as Express from "express";
 import { ENV } from "./helpers";
 import { Endpoints } from "./endpoints";
 import { iEndpoint } from "./interfaces";
+import { Database } from "./database";
 
 (() => {
   ENV.initialize();
+  Database.initialize("sqlite::memory:");
 
   const application = Express();
 
   Endpoints.forEach((endpoint: iEndpoint) => {
     const method = endpoint.method.toLocaleLowerCase();
 
-    method === "get" ? application.get(endpoint.url, endpoint.handdler) : false;
-    method === "post" ? application.post(endpoint.url, endpoint.handdler) : false;
-    method === "put" ? application.put(endpoint.url, endpoint.handdler) : false;
-    method === "delete" ? application.delete(endpoint.url, endpoint.handdler) : false;
+    if (method === "get") 
+      application.get(endpoint.url, endpoint.handdler);
+    if (method === "post") 
+      application.post(endpoint.url, endpoint.handdler);
+    if (method === "put") 
+      application.put(endpoint.url, endpoint.handdler);
+    if (method === "delete")
+      application.delete(endpoint.url, endpoint.handdler);
   });
 
   application.listen(3000);
