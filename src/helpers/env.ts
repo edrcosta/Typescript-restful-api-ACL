8b204954affa34d74413b2ef0a01a326ad6a7ef1
Eder Costa <edercosta@oulook.com>
2021-03-13 13:38:14 -0300

    simplify env helper

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -1,12 +1,5 @@
 import { iEnv } from '../interfaces'
 
 export class ENV {
-  static initialize(): iEnv {
-    const envFile = require('../../env.json')
-
-    return {
-      SERVER_PORT: envFile.SERVER_PORT || 3000,
-      DATABASE_CONNECTION_STRING: envFile.DATABASE_CONNECTION_STRING || 'sqlite::memory:',
-    }
-  }
+  static initialize = (): iEnv => require('../../env.json')
 }
--- src/interfaces/env.ts
+++ src/interfaces/env.ts
@@ -1,5 +1,12 @@
-export interface iEnv{
-    SERVER_PORT: string
-    DATABASE_CONNECTION_STRING: string
-  }
-  
+export interface iEnvDatabase {
+  SCHEMA: string
+  HOST: string
+  PASSWORD: string
+  USER: string
+  DIALECT: 'mysql' | 'mariadb' | 'postgres' | 'mssql'
+}
+
+export interface iEnv {
+  SERVER_PORT: string
+  DATABASE: iEnvDatabase
+}
