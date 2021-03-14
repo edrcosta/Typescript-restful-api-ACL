21c0d1c81cbde612e2fd1de7ff578c10d45f36f9
Eder Costa <edercosta@oulook.com>
2021-03-13 22:35:18 -0300

    body-parser json

--- src/start.ts
+++ src/start.ts
@@ -1,17 +1,22 @@
 import * as Express from 'express'
+import { urlencoded, json } from 'body-parser'
 
 import { ENV } from './helpers'
 import { LoadEndpoints } from './endpoints'
 import { Database } from './database'
-
-(() => {
+;(() => {
   const env = ENV.initialize()
-  
-  Database.initialize(env.DATABASE_CONNECTION_STRING)
+
+  Database.initialize(env.DATABASE)
 
   const application = Express()
-  
+
+  application.use(urlencoded({ extended: false }))
+  application.use(json())
+
   LoadEndpoints(application)
 
   application.listen(env.SERVER_PORT)
+
+  console.log(`Server running on port ${env.SERVER_PORT}`)
 })()
