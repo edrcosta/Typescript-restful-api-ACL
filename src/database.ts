0a4088d946bc6b43e6a18d1209b100e5d9ce3baa
Eder Costa <edercosta@oulook.com>
2021-03-13 22:38:43 -0300

    adding return values database.ts

--- src/database.ts
+++ src/database.ts
@@ -1,32 +1,31 @@
 import { Sequelize } from 'sequelize'
-import { 
-  iUserSchema, 
-  iUserAddSchema, 
-  UserModelSchema,
+import { UserModelSchema, UserTypeModelSchema } from './models'
+import {
+  iEnvDatabase,
+  iTableMap,
   iUserTypeSchema,
   iUserTypeAddSchema,
-  UserTypeModelSchema
-} from './models'
-
-import { iEnvDatabase, iTableMap } from './interfaces'
+  iUserSchema,
+  iUserAddSchema,
+} from './interfaces'
 
 export class Database {
   static connection: Sequelize
   static tables: iTableMap
 
-  static initialize(envDb: iEnvDatabase) {
+  static initialize(envDb: iEnvDatabase): void {
     const sequelize = new Sequelize(envDb.SCHEMA, envDb.USER, envDb.PASSWORD, {
       host: envDb.HOST,
       dialect: envDb.DIALECT,
-      logging: false
+      logging: false,
     })
 
     Database.tables = {
-      users: sequelize.define<iUserSchema, iUserAddSchema>(
+      Users: sequelize.define<iUserSchema, iUserAddSchema>(
         'Users',
         UserModelSchema
       ),
-      userTypes: sequelize.define<iUserTypeSchema, iUserTypeAddSchema>(
+      UserTypes: sequelize.define<iUserTypeSchema, iUserTypeAddSchema>(
         'UserTypes',
         UserTypeModelSchema
       ),
