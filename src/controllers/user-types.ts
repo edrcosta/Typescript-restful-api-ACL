9b59e5f0b557cb566dbd0487556a5b81d8a54a84
Eder Costa <edercosta@oulook.com>
2021-03-15 17:55:10 -0300

    adding types to usertypes endpoints

--- src/controllers/user-types.ts
+++ src/controllers/user-types.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from 'express'
 import { CRUD } from '../bussiness/crud'
-import { iUserTypeGetOneParams, iUserTypeListQuery } from '../interfaces'
+import { iUserTypeGetOneParams, iUserTypeListQuery, iUserTypeSchema } from '../interfaces'
 import { Database } from '../bussiness/database'
 
 export class UserTypesController {
   /**
    * Endpoint to get an User Type by id
    */
-  async get(req: Request<iUserTypeGetOneParams>, res: Response): Promise<void> {
-    res.json(
+  async get(req: Request<iUserTypeGetOneParams>, res: Response): Promise<Response<null | iUserTypeSchema>> {
+    return res.json(
       await Database.tables.UserTypes.findOne({
         where: { id: req.params.id },
         attributes: { exclude: ['deleted'] },
@@ -19,9 +19,9 @@ export class UserTypesController {
   /**
    * Endpoint to list User Types with pagination
    */
-  async list(req: Request<null, null, null, iUserTypeListQuery>, res: Response): Promise<void> {
+  async list(req: Request<null, null, null, iUserTypeListQuery>, res: Response): Promise<Response<iUserTypeSchema[]>> {
     const userTypes = new CRUD('UserTypes')
 
-    res.json(await userTypes.listWithPagination(req.query.page ? req.query.page : 0, ['deleted']))
+    return res.json(await userTypes.listWithPagination(req.query.page ? req.query.page : 0, ['deleted']))
   }
 }
