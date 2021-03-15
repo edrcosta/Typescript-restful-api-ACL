01ea11b35d5dbc3275fe57a9888a7f2dc74a4a05
Eder Costa <edercosta@oulook.com>
2021-03-15 17:52:06 -0300

    types for endpoint returns

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,20 +2,33 @@ import { Request, Response } from 'express'
 import { CRUD, Authentication } from '../bussiness'
 import { Database } from '../bussiness/database'
 import { UserModel } from '../models'
-import { iUserListQuery, iUserGetOneParams, iUserAddSchema, iUserDeleteParams, iUserUpdateBodySchema, iUserUpdateParams } from '../interfaces'
+import {
+  iUserListQuery,
+  iUserGetOneParams,
+  iUserAddSchema,
+  iUserDeleteParams,
+  iUserUpdateBodySchema,
+  iUserUpdateParams,
+  iUserSchema,
+  iAuthResponse,
+  iValidateErrors,
+  iUserUpdateResponse,
+  iUserDeletedResponse,
+} from '../interfaces'
 
 export class UsersController {
   /**
    * Endpoint to get an user by id
    */
-  async get(req: Request<iUserGetOneParams>, res: Response): Promise<void> {
+  async get(req: Request<iUserGetOneParams>, res: Response): Promise<Response<iUserSchema>> {
     const auth = new Authentication()
+
     let findId = req.params.id
     if (Authentication.loggedData.userType === auth.userTypesEnum.geral) {
       findId = Authentication.loggedData.id
     }
 
-    res.json(
+    return res.json(
       await Database.tables.Users.findOne({
         where: {
           ...Database.softDelete,
@@ -29,96 +42,84 @@ export class UsersController {
   /**
    * Endpoint to List Users with pagination
    */
-  async list(req: Request<null, null, null, iUserListQuery>, res: Response): Promise<void> {
+  async list(req: Request<null, null, null, iUserListQuery>, res: Response): Promise<Response<iUserSchema[]>> {
     const auth = new Authentication()
 
     if (!auth.userHasPermission(req.headers.authorization, ['admin', 'root'])) {
-      res.json(auth.authenticationError)
-    } else {
-      res.json(await UserModel.listWithPagination(req.query.page ? req.query.page : 0))
+      return res.json(auth.authenticationError)
     }
+    return res.json(await UserModel.listWithPagination(req.query.page ? req.query.page : 0))
   }
 
   /**
    * Endpoint to Create new User
    */
-  async create(req: Request<null, null, iUserAddSchema>, res: Response): Promise<void> {
+  async create(req: Request<null, null, iUserAddSchema>, res: Response): Promise<Response<iUserSchema | iValidateErrors | iAuthResponse>> {
     const auth = new Authentication()
 
     if (!auth.userHasPermission(req.headers.authorization, ['admin', 'root'])) {
-      res.json(auth.authenticationError)
-    } else {
-      const users = new CRUD('Users')
-
-      const errors = await users.validate<iUserAddSchema>(req.body)
-
-      if (errors) {
-        res.json({
-          errors: errors,
-        })
-      } else {
-        const exists = await UserModel.checkExist(req.body.email)
-
-        if (exists === null) {
-          res.json(await UserModel.create(req.body))
-        } else {
-          res.json({
-            errors: ['this user aready exists'],
-          })
-        }
-      }
+      return res.json(auth.authenticationError)
     }
+
+    const users = new CRUD('Users')
+    const errors = await users.validate<iUserAddSchema>(req.body)
+
+    if (errors) return res.json({ errors: errors })
+
+    const exists = await UserModel.checkExist(req.body.email)
+
+    if (exists === null) return res.json(await UserModel.create(req.body))
+
+    return res.json({
+      errors: ['this user aready exists'],
+    })
   }
 
   /**
    * Endpoint to update an user by id
    */
-  async update(req: Request<iUserUpdateParams, null, iUserUpdateBodySchema>, res: Response): Promise<void> {
+  async update(req: Request<iUserUpdateParams, null, iUserUpdateBodySchema>, res: Response): Promise<Response<iUserUpdateResponse | iAuthResponse | iValidateErrors>> {
     const auth = new Authentication()
 
     if (!auth.userHasPermission(req.headers.authorization, ['admin', 'root'])) {
-      res.json(auth.authenticationError)
-    } else {
-      const users = new CRUD('Users')
-      const errors = await users.validate<iUserUpdateBodySchema>(req.body, true)
-
-      if (errors) {
-        res.json(errors)
-      } else {
-        const updated = await Database.tables.Users.update(req.body, {
-          where: {
-            id: req.params.id,
-          },
-        })
-
-        res.json({
-          updated: updated[0] === 1,
-          data: req.body,
-        })
-      }
+      return res.json(auth.authenticationError)
     }
+
+    const users = new CRUD('Users')
+    const errors = await users.validate<iUserUpdateBodySchema>(req.body, true)
+
+    if (errors) return res.json(errors)
+
+    const updated = await Database.tables.Users.update(req.body, {
+      where: { id: req.params.id },
+    })
+
+    return res.json({
+      updated: updated[0] === 1,
+      data: req.body,
+    })
   }
 
   /**
    * Endpoint to Soft delete a user
    */
-  async delete(req: Request<iUserDeleteParams>, res: Response): Promise<void> {
+  async delete(req: Request<iUserDeleteParams>, res: Response): Promise<Response<iValidateErrors | iUserDeletedResponse>> {
     const auth = new Authentication()
 
     if (!auth.userHasPermission(req.headers.authorization, ['root'])) {
-      res.json(auth.authenticationError)
-    } else {
-      const updated = await Database.tables.Users.update(
-        { deleted: true },
-        {
-          where: {
-            ...Database.softDelete,
-            id: req.params.id,
-          },
-        }
-      )
-
-      res.json({ deleted: updated[0] === 1 })
+      return res.json(auth.authenticationError)
     }
+
+    const updated = await Database.tables.Users.update(
+      { deleted: true },
+      {
+        where: {
+          ...Database.softDelete,
+          id: req.params.id,
+        },
+      }
+    )
+
+    return res.json({ deleted: updated[0] === 1 })
   }
 }
--- src/interfaces/controllers/users.ts
+++ src/interfaces/controllers/users.ts
@@ -13,3 +13,16 @@ export interface iUserUpdateParams {
 export interface iUserDeleteParams {
   id: number
 }
+
+export interface iValidateErrors {
+  errors: string[]
+}
+
+export interface iUserUpdateResponse {
+  updated: boolean
+  data: iUserUpdateParams
+}
+
+export interface iUserDeletedResponse {
+  deleted: boolean
+}
