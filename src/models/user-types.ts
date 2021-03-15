1b738e849c9f97973d4067e405aee330f887f248
Eder Costa <edercosta@oulook.com>
2021-03-15 01:06:50 -0300

    moving some methods to they own models

--- src/models/user-types.ts
+++ src/models/user-types.ts
@@ -1,24 +1,26 @@
 import { DataTypes } from 'sequelize'
 
-export const UserTypeModelSchema = {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-  },
-  description: {
-    type: DataTypes.STRING,
-  },
-  createdAt: {
-    field: 'created_at',
-    type: DataTypes.STRING,
-  },
-  updatedAt: {
-    field: 'updated_at',
-    type: DataTypes.STRING,
-  },
-  deleted: DataTypes.BOOLEAN,
+export class UserTypesModel {
+  static schema = {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      field: 'created_at',
+      type: DataTypes.STRING,
+    },
+    updatedAt: {
+      field: 'updated_at',
+      type: DataTypes.STRING,
+    },
+    deleted: DataTypes.BOOLEAN,
+  }
 }
--- src/models/users.ts
+++ src/models/users.ts
@@ -1,44 +1,94 @@
 import { DataTypes } from 'sequelize'
+import { Database } from '../bussiness'
+import { CryptoHelper, LOG } from '../helpers'
+import { iUserAddSchema, iUserSchema } from '../interfaces'
 
-export const UserModelSchema = {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  typeId: {
-    field: 'type_id',
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  status: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  passwordSalt: {
-    field: 'password_salt',
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  createdAt: {
-    field: 'created_at',
-    type: DataTypes.DATE,
-  },
-  updatedAt: {
-    field: 'updated_at',
-    type: DataTypes.DATE,
-  },
-  deleted: DataTypes.BOOLEAN,
+export class UserModel {
+  static schema = {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    typeId: {
+      field: 'type_id',
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    status: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    passwordSalt: {
+      field: 'password_salt',
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      field: 'created_at',
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      field: 'updated_at',
+      type: DataTypes.DATE,
+    },
+    deleted: DataTypes.BOOLEAN,
+  }
+
+  static async create(user: iUserAddSchema): Promise<boolean> {
+    try {
+      const pass = CryptoHelper.getPasswordHash(user.password)
+
+      user.password = pass.hash
+      user.passwordSalt = pass.salt
+
+      const result = await Database.tables.Users.create(user)
+
+      if (result.id) return true
+    } catch (error) {
+      LOG.error(error)
+      return false
+    }
+    return false
+  }
+
+  static async checkExist(email: string): Promise<iUserSchema | null> {
+    return Database.tables.Users.findOne({
+      where: {
+        ...Database.softDelete,
+        email: email,
+        status: 'ATIVO',
+      },
+      include: [Database.tables.UserTypes],
+    })
+  }
+
+  static listWithPagination(page: number): Promise<iUserSchema[]> {
+    const perPage = 10
+    return Database.tables.Users.findAll({
+      where: {
+        deleted: null,
+        status: 'ATIVO',
+      },
+      limit: perPage,
+      attributes: {
+        exclude: ['passwordSalt', 'password', 'deleted'],
+      },
+      include: [Database.tables.UserTypes],
+      offset: (page + 1) * perPage - perPage,
+    })
+  }
 }
